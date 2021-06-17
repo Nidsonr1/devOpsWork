164801ddc1f15731d464716244615b415525dfb8
Nidsonr1 <nidsoncampos@hotmail.com>
2021-06-17 01:05:23 -0300

    Agora vai

--- server/src/server.js
+++ server/src/server.js
@@ -8,4 +8,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => { console.log(`Server is run in http://54.94.10.56:3333:${port}`) });
+app.listen(port, () => { console.log(`Server is run in http://54.94.184.84:3333:${port}`) });
